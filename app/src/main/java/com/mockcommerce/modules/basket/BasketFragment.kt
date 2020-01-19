ab91b5ea2acdb8275a6e4ea1e93e0360aa0109d5
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2020-01-19 18:30:40 +0300

    Added total to basket. Extracted model logic to viewmodel

--- app/src/main/java/com/mockcommerce/modules/basket/BasketFragment.kt
+++ app/src/main/java/com/mockcommerce/modules/basket/BasketFragment.kt
@@ -11,10 +11,9 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mockcommerce.R
-import com.mockcommerce.modules.shared.product_list.ProductListItemAdapter
 import kotlinx.android.synthetic.main.basket_fragment.view.*
-import timber.log.Timber
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class BasketFragment : Fragment() {
 
@@ -39,41 +38,29 @@ class BasketFragment : Fragment() {
         basketRecycler.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         basketRecycler.addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
         basketAdapter = BasketListAdapter() {product, action ->
-            val basket_temp = viewModel.basketItems.value
-            val index = basket_temp!!.indexOf(product)
-
             when(action) {
                 ADAPTER_ACTION.DELETE -> {
-                    basket_temp.remove(product)
+                    viewModel.removeFromBasket(product)
 
                 }
                 ADAPTER_ACTION.ADD -> {
-                    product.numbersInBasket += 1
-                    basket_temp[index] = product
+                    viewModel.add(product)
                 }
                 ADAPTER_ACTION.SUBSTRACT -> {
-                    product.numbersInBasket -= 1
-                    if(product.numbersInBasket == 0){
-                        product.numbersInBasket = 1
-                    }
-                    basket_temp[index] = product
+                    viewModel.substract(product)
                 }
                 ADAPTER_ACTION.POSTPONE -> {
-                    basket_temp.remove(product)
-                    val post_temp = viewModel.postponedItems.value
-                    post_temp!!.add(product)
-                    viewModel.postponedItems.postValue(post_temp)
+                    viewModel.postpone(product)
                 }
                 ADAPTER_ACTION.VIEW -> {
-                    val action = BasketFragmentDirections.actionBasketToProduct2(product.id )
+                    val action = BasketFragmentDirections.actionBasketToProduct2(product.id)
                     findNavController().navigate(action)
                 }
 
                 else -> Timber.d("Invalid operation %i", action)
             }
-
-            viewModel.basketItems.postValue(basket_temp)
         }
+
         basketRecycler.adapter = basketAdapter
 
         viewModel.basketItems.observe(this.viewLifecycleOwner, Observer { t ->
@@ -85,36 +72,27 @@ class BasketFragment : Fragment() {
         postponedAdapter = PostponedListAdapter { product, action ->
             when (action){
                 ADAPTER_ACTION.ADD_TO_CART -> {
-                    val post_temp = viewModel.postponedItems.value
-                    if(post_temp != null) {
-                        post_temp.remove(product)
-                        viewModel.postponedItems.postValue(post_temp)
-                    }
-
-                    val basket_temp = viewModel.basketItems.value
-                    if(basket_temp != null) {
-                        basket_temp.add(product)
-                        viewModel.basketItems.postValue(basket_temp)
-                    }
+                    viewModel.addToBasket(product)
                 }
                 ADAPTER_ACTION.DELETE -> {
-                    val post_temp = viewModel.postponedItems.value
-                    if(post_temp != null) {
-                        post_temp.remove(product)
-                        viewModel.postponedItems.postValue(post_temp)
-                    }
+                    viewModel.removeFromPostPoned(product)
                 }
                 ADAPTER_ACTION.VIEW -> {
-                    val action = BasketFragmentDirections.actionBasketToProduct2(product.id )
+                    val action = BasketFragmentDirections.actionBasketToProduct2(product.id)
                     findNavController().navigate(action)
                 }
                 else -> Timber.d("Invalid operation %i", action)
             }
         }
+        
         postponedRecycler.adapter = postponedAdapter
 
         viewModel.postponedItems.observe(this.viewLifecycleOwner, Observer { t ->
             postponedAdapter.updateItems(t)
+        })
+
+        viewModel.basketTotal.observe(this.viewLifecycleOwner, Observer { t ->
+            v.basket_total.text = t
         })
 
         return v
--- app/src/main/java/com/mockcommerce/modules/basket/BasketViewModel.kt
+++ app/src/main/java/com/mockcommerce/modules/basket/BasketViewModel.kt
@@ -10,12 +10,75 @@ class BasketViewModel(val appRepository: AppRepository) : ViewModel() {
 
     var postponedItems: MutableLiveData<ArrayList<ProductModel>> = MutableLiveData(ArrayList())
 
+    var basketTotal: MutableLiveData<String> = MutableLiveData("0 TL")
+
     init {
         appRepository.getBasket {
             basketItems.postValue(it)
+            calculateTotal(it)
         }
         appRepository.getBasketPostponed {
             postponedItems.postValue(it)
         }
     }
+
+    fun calculateTotal(products: ArrayList<ProductModel>) {
+        var total: Float = 0.0F
+
+        products.forEach {
+            total += it.numbersInBasket * it.price
+        }
+
+        basketTotal.postValue("Tutar %.2f TL".format(total))
+    }
+
+    fun removeFromPostPoned(product: ProductModel) {
+        val temp = postponedItems.value
+        temp!!.remove(product)
+        postponedItems.postValue(temp)
+    }
+
+    fun removeFromBasket(product: ProductModel) {
+        val temp = basketItems.value
+        temp!!.remove(product)
+        basketItems.postValue(temp)
+        calculateTotal(temp)
+    }
+
+    fun postpone(product: ProductModel) {
+        val temp = postponedItems.value
+        product.numbersInBasket = 1
+        temp!!.add(product)
+        postponedItems.postValue(temp)
+        removeFromBasket(product)
+    }
+
+    fun addToBasket(product: ProductModel) {
+        val temp = basketItems.value
+        product.numbersInBasket = 1
+        temp!!.add(product)
+        basketItems.postValue(temp)
+        removeFromPostPoned(product)
+        calculateTotal(temp)
+    }
+
+    fun add(product: ProductModel) {
+        val temp = basketItems.value
+        val index = temp!!.indexOf(product)
+        product.numbersInBasket += 1
+        temp[index] = product
+        basketItems.postValue(temp)
+        calculateTotal(temp)
+    }
+
+    fun substract(product: ProductModel) {
+        val temp = basketItems.value
+        val index = temp!!.indexOf(product)
+        product.numbersInBasket =
+            if (product.numbersInBasket == 1) 1 else product.numbersInBasket - 1
+        temp[index] = product
+        basketItems.postValue(temp)
+        calculateTotal(temp)
+    }
+
 }
